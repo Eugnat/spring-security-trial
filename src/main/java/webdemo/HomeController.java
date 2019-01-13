e113d00163bcc1492bea1228ef06eee1f16da68f
Yevgeny Zazdravnykh <yevgeny.zazdravnykh@gmail.com>
2019-01-13 18:05:27 +0200

    Current state with XML security configuration

---
+++ src/main/java/webdemo/HomeController.java
@@ -0,0 +1,23 @@
+package webdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("/")
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/trial")
+    public String trial() {
+        return "trial";
+    }
+
+    @GetMapping("/servlet")
+    public String servlet() {
+        return "servlet";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/trial.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Trial page</title>
+</head>
+<body>
+<h1>Trial line</h1>
+</body>
+</html>

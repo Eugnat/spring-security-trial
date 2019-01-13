601e623469406a7971927ddae33e655187d42df4
Yevgeny Zazdravnykh <yevgeny.zazdravnykh@gmail.com>
2019-01-13 18:51:37 +0200

    Configuration without xml, only JavaConfig

---
+++ src/main/java/webdemo/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package webdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                                .antMatchers("/servlet", "/trial").authenticated()
+                                .and()
+                                .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password(encoder().encode("user1")).authorities("ROLE_USER")
+                .and()
+                .withUser("user2").password(encoder().encode("user2")).authorities("ROLE_ADMIN");
+    }
+}

e9d792736d2068f90e76eb365aa62e0ce064dfb6
Yevgeny Zazdravnykh <yevgeny.zazdravnykh@gmail.com>
2019-02-01 01:09:36 +0200

    Small updates

--- src/main/java/webdemo/controllers/HomeController.java
+++ src/main/java/webdemo/controllers/HomeController.java
@@ -1,23 +1,27 @@
 package webdemo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("/")
 public class HomeController {
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("trialLine", "Trial line");
         return "index";
     }
 
     @GetMapping("/trial")
-    public String trial() {
+    public String trial(Model model) {
+        model.addAttribute("trialLine", "Trial line");
         return "trial";
     }
 
     @GetMapping("/servlet")
-    public String servlet() {
+    public String servlet(Model model) {
+        model.addAttribute("trialLine", "Trial line");
         return "servlet";
     }
 }

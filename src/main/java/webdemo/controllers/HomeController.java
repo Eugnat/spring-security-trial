d06ca251b5b5d7d299b1feb8ea88cd41cd76d1fc
Yevgeny Zazdravnykh <yevgeny.zazdravnykh@gmail.com>
2019-02-02 21:16:45 +0200

    Added display of user name as authenticated

--- src/main/java/webdemo/controllers/HomeController.java
+++ src/main/java/webdemo/controllers/HomeController.java
@@ -1,16 +1,28 @@
 package webdemo.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
 
 @Controller("/")
 public class HomeController {
 
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {
+            model.addAttribute("trial", authentication.getName());
+        }
+        else {
+            model.addAttribute("trial", "No user");
+        }
+
         return "index";
     }
 
